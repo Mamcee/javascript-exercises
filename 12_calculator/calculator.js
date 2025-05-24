da766878ce48ab020958f90910a5740ed6104645
Mamcee <150480958+Mamcee@users.noreply.github.com>
2025-05-24 05:34:52 -0500

    Update calculator.js

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,24 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+return num1 + num2;	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+return num1 - num2;	
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let sum = 0;
+	for(let i =0; i< numbers.length; i ++){
+		sum += numbers[i];
+	}
+	return sum;
 };
 
-const multiply = function() {
 
+const multiply = function(num1, num2) {
+return num1 * num2;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
+const factorial = function(number) {
+	if (number < 0){
+		return "factorial is not defined for negative numbers";}
+	if (number === 0){
+		return 1;
+	}
+	return number * factorial (number -1);
 	
 };
 
