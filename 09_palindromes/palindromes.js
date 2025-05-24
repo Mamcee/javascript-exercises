f7493eba73e6917bd0873c8737eeff78c4f1cf35
Mamcee <150480958+Mamcee@users.noreply.github.com>
2025-05-24 06:41:16 -0500

    Update palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+const cleanstr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  const reversestr = cleanstr.split(''). reverse(). join('');
+  return cleanstr === reversestr;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+  
