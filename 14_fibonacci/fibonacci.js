2896fdbb69b0474c86cddaff9a2d4187b1591907
Mamcee <150480958+Mamcee@users.noreply.github.com>
2025-05-24 15:02:57 -0500

    Update fibonacci.js

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n <= 0) return [];
+let fib = [0, 1];
+  for (let i = 2; i < n; i++){
+    fib[i]= fib[i-1] + fib [i-2];}
+  return fib.slice (0, n);
 };
 
 // Do not edit below this line
