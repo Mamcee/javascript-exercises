6376e8192ba581dab694e2e4ea74cbaa04372986
Mamcee <150480958+Mamcee@users.noreply.github.com>
2025-05-25 11:23:31 -0500

    Update getTheTitles.js

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,15 @@
-const getTheTitles = function() {
+const books = [
+  { title: "The Hobbit", author: "J.R.R. Tolkien" },
+  { title: "1984", author: "George Orwell" },
+  { title: "Pride and Prejudice", author: "Jane Austen" }
+];
 
+const getTheTitles = function(booksArray) {
+  return booksArray.map(book => book.title);
 };
+
+const titles = getTheTitles(books);
+console.log(titles); // ["The Hobbit", "1984", "Pride and Prejudice"]
 
 // Do not edit below this line
 module.exports = getTheTitles;
